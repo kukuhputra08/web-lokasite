4723b7e1656dc1efafb397a6d3ab15bbb157fb33
kukuhputra08 <149924715+kukuhputra08@users.noreply.github.com>
2026-05-31 23:03:08 +0700

    Initial commit LokaSite

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 
 import Home from "../pages/Home";
 import Services from "../pages/Services";
@@ -8,20 +8,19 @@ import OrderSuccess from "../pages/OrderSuccess";
 import AdminLogin from "../pages/AdminLogin";
 import AdminDashboard from "../pages/AdminDashboard";
 import AdminOrderDetail from "../pages/AdminOrderDetail";
+
 import ProtectedRoute from "./ProtectedRoute";
-import SupabaseTest from "../pages/SupabaseTest";
 
 function AppRoutes() {
   return (
     <Routes>
-      {/* Customer Pages */}
       <Route path="/" element={<Home />} />
       <Route path="/services" element={<Services />} />
       <Route path="/pricing" element={<Pricing />} />
       <Route path="/order" element={<Order />} />
       <Route path="/order/success" element={<OrderSuccess />} />
-      <Route path="/supabase-test" element={<SupabaseTest />} />
-      {/* Admin Pages */}
+
+      <Route path="/admin" element={<Navigate to="/admin/login" replace />} />
       <Route path="/admin/login" element={<AdminLogin />} />
 
       <Route
